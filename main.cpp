f59af017a825ad3cfd6f5970c8b0e73d54c7828e
Yun Jang <jang.yunj7@gmail.com>
2023-03-25 11:42:58 -0400

    search element in array

--- main.cpp
+++ main.cpp
@@ -1,23 +1,35 @@
 #include <iostream>
-
-double getTotal(double prices[], int size);
+int searchArray(int numbers[], int size, int myNum);
 
 int main()
 {
-    double prices[] = {49.99, 15.05, 75, 9.99};
-    int size = sizeof(prices) / sizeof(prices[0]); // explicitly calculate the size of the array
-    double total = getTotal(prices, size);
+    int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int size = sizeof(numbers) / sizeof(int);
+    int index;
+    int myNum;
 
-    std::cout << "$" << total;
+    std::cout << "Enter element to search: " << '\n';
+    std::cin >> myNum;
+
+    index = searchArray(numbers, size, myNum);
+    if (index != -1)
+    {
+        std::cout << myNum << " is at index " << index;
+    }
+    else
+    {
+        std::cout << myNum << " is not in the array";
+    }
 }
 
-double getTotal(double prices[], int size) // when iterating over an array, you need second var that informs the size of an array
+int searchArray(int numbers[], int size, int myNum)
 {
-    double total = 0; // local variable - it can be same name as long as it's in a different function
-
     for (int i = 0; i < size; i++)
     {
-        total += prices[i];
+        if (numbers[i] == myNum)
+        {
+            return i;
+        }
     }
-    return total;
+    return -1;
 }
