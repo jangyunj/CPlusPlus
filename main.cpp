f06fa5a716b2a235043ab89fdc0435eeaa98746a
Yun Jang <jang.yunj7@gmail.com>
2023-03-28 23:51:34 -0400

    second commit

--- main.cpp
+++ main.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 
 int main()
-{
-    std::string foods[100];
+{ //---FILL 50 WITH PIZZA & 50 WITH BURGER---
+    const int size = 100;
+    std::string foods[size];
 
-    fill(foods, foods + 100, "pizza");
+    fill(foods, foods + (size / 2), "pizza");
+    fill(foods + (size / 2), foods + size, "hamburger");
 
     for (std::string food : foods)
     {
         std::cout << food << '\n';
     }
+
+    //---FILL ALL 100 WITH PIZZA---
+    // std::string foods[100];
+
+    // fill(foods, foods + 100, "pizza");
+
+    // for (std::string food : foods)
+    // {
+    //     std::cout << food << '\n';
+    // }
 }
