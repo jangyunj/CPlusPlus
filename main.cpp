9e203a834b429bca5568971f9cfd6bfd2b4b6a9c
Yun Jang <jang.yunj7@gmail.com>
2023-03-29 23:30:48 -0400

    fill integers

--- main.cpp
+++ main.cpp
@@ -2,19 +2,36 @@
 
 int main()
 {
-    //---FILL 1/3 WITH PIZZA, BURGER, HOTDOG-------------------
-    const int size = 99;
-    std::string foods[size];
+    //---FILL 1/4 WITH 1, 2, 3, 4 (INTEGERS)--------------------
+    const int size = 100;
+    int numbers[size];
 
-    fill(foods, foods + (size / 3), "pizza");
-    fill(foods + (size / 3), foods + (size / 3) * 2, "burger");
-    fill(foods + (size / 3) * 2, foods + size, "hotdog");
+    std::fill(numbers, numbers + 25, 1);
+    std::fill(numbers + 25, numbers + 50, 2);
+    std::fill(numbers + 50, numbers + 75, 3);
+    std::fill(numbers + 75, numbers + 100, 4);
 
-    for (std::string food : foods)
+    for (int number : numbers)
     {
-        std::cout << food << '\n';
+        std::cout << number << '\n';
     }
 }
+
+
+// {
+//     //---FILL 1/3 WITH PIZZA, BURGER, HOTDOG-------------------
+//     const int size = 99;
+//     std::string foods[size];
+
+//     fill(foods, foods + (size / 3), "pizza");
+//     fill(foods + (size / 3), foods + (size / 3) * 2, "burger");
+//     fill(foods + (size / 3) * 2, foods + size, "hotdog");
+
+//     for (std::string food : foods)
+//     {
+//         std::cout << food << '\n';
+//     }
+// }
 
 // { //---FILL 50 WITH PIZZA & 50 WITH BURGER---
 //     const int size = 100;
