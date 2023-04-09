299752af98a9ab37c0b2714029fa14c06dba6714
Yun Jang <jang.yunj7@gmail.com>
2023-04-09 14:42:48 -0400

    swap variables using memory address

--- main.cpp
+++ main.cpp
@@ -1,26 +1,22 @@
 #include <iostream>
 
+void swap(std::string &x, std::string &y);
+
 int main()
 {
     std::string x = "Kool-aid";
     std::string y = "Water";
-    std::string temp;
 
-    temp = x;
-    x = y;
-    y = temp;
+    swap(x, y);
 
     std::cout << "X: " << x << '\n';
     std::cout << "Y: " << y << '\n';
+}
 
-    int a = 90;
-    int b = 91;
-    int t;
-
-    t = a;
-    a = b;
-    b = t;
-
-    std::cout << a << '\n';
-    std::cout << b << '\n';
+void swap(std::string &x, std::string &y)
+{
+    std::string temp;
+    temp = x;
+    x = y;
+    y = temp;
 }
