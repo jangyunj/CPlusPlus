39591c85cf1fa832e2bcc199dbdc1df80338f44e
Yun Jang <jang.yunj7@gmail.com>
2023-04-16 15:38:21 -0400

    dynamic memory

--- main.cpp
+++ main.cpp
@@ -1,22 +1,17 @@
+/*dynamic memory - memory that is allocated after the program is already compiled and running
+                 - use 'new' operator to allocate memory in the HEAP
+                 - useful when we don't know how much memory we'll need; more flexible */
+
 #include <iostream>
 
 int main()
 {
-    // Nullptr = keyword that represents a null pointer literal
-    // Nullptr : helpful when determining if an address was successfully assigned to a pointer
+    int *pNum = NULL;
+    pNum = new int;
+    *pNum = 21;
+    std::cout << "address: " << pNum << '\n';
+    std::cout << "value: " << *pNum << '\n';
 
-    int *pointer = nullptr;
-    int x = 123;
-
-    pointer = &x;
-
-    if (pointer == nullptr)
-    {
-        std::cout << "address was not assigned\n";
-    }
-    else
-    {
-        std::cout << "address was assigned\n";
-        std::cout << *pointer;
-    }
+    delete pNum; // freeing up the memory at this location; otherwise, it may cause memory leak
+                 //  when using 'new' operator, try to use 'delete' operator too
 }
